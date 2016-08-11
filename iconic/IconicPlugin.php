240d13ebd18cef91b0e6ecdf34e3c227bfbd3ea4
Nicholas O'Donnell <nicholas.odonnell@picdorsey.com>
2016-08-11 14:16:49 -0400

    Update description

--- iconic/IconicPlugin.php
+++ iconic/IconicPlugin.php
@@ -40,7 +40,7 @@ class IconicPlugin extends BasePlugin
      */
     public function getDescription()
     {
-        return Craft::t('Adds Font Awesome Icons to Redactor II.');
+        return Craft::t('Replaces words with Font Awesome icons on Redactor II.');
     }
 
     /**
